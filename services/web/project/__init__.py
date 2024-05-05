374cdeeef7b1186add39ef5f3248fc186fe226f9
justinchiao <justinc.usatf@gmail.com>
2024-05-04 21:27:55 -0700

    routes?

--- services/web/project/__init__.py
+++ services/web/project/__init__.py
@@ -4,6 +4,7 @@ from flask import (
     jsonify,
     send_from_directory,
     request,
+    render_template
 )
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
@@ -13,6 +14,8 @@ app = Flask(__name__)
 app.config.from_object("project.config.Config")
 db = SQLAlchemy(app)
 
+def are_creds_good(user,pass):
+    #look into db and find 
 
 class User(db.Model):
     __tablename__ = "users"
@@ -26,8 +29,8 @@ class User(db.Model):
 
 
 @app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+def hello_world()
+    return render_template('root.html') 
 
 
 @app.route("/static/<path:filename>")
@@ -39,17 +42,24 @@ def staticfiles(filename):
 def mediafiles(filename):
     return send_from_directory(app.config["MEDIA_FOLDER"], filename)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    username=request.form.get("username")
+    password=request.form.get("password")
+    
+    good_credentials = are_creds_good(username,password)
+    
+    if username is None:
+        return render_template('login.html', bad_credentials=False)
+    else:
+        if not good_credentials:
+            return render_template('login.html', bad_credentials=True)
+        else:
+            return render_template('login.html', bad_credentials=False)
 
-@app.route("/upload", methods=["GET", "POST"])
-def upload_file():
-    if request.method == "POST":
-        file = request.files["file"]
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config["MEDIA_FOLDER"], filename))
-    return """
-    <!doctype html>
-    <title>upload new File</title>
-    <form action="" method=post enctype=multipart/form-data>
-        <p><input type=file name=file><input type=submit value=Upload>
-    </form>
-    """
+    return render_template('login.html')
+
+@app.route("/logout", methods=["GET", "POST"])
+@app.route("/create_account", methods=["GET", "POST"])
+@app.route("/create_message", methods=["GET", "POST"])
+@app.route("/search", methods=["GET", "POST"])
